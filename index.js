7c46c65df3b7fad6970c5330ea45b2e30993e837
tr1ckste <nikitavasilinenko@gmail.com>
2022-01-08 13:55:11 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('hi ksta');
+});
+
+app.listen(PORT, () => {
+  console.log('app listening at http://localhost:' + PORT);
+})
