a0f542507048675e6c5f1e94ff9345b47c9b6307
tr1ckste <nikitavasilinenko@gmail.com>
2022-01-09 13:16:02 +0200

    added mongo

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 'use strict';
 
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config({ path: './.env' });
 
+const app = express();
 const PORT = 3000;
+
+const { DB_PASS, DB_LOGIN, DB_NAME } = process.env;
+const mongoDB = `mongodb+srv://${DB_LOGIN}:${DB_PASS}@cluster0.kaz3t.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
 app.get('/', (req, res) => {
   res.send('hi ksta');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await mongoose.connect(mongoDB);
   console.log('app listening at http://localhost:' + PORT);
 });
